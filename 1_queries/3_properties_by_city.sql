402e49722230820a24a8faeb3ad0fa576b9b2552
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-04-01 17:13:09 -0700

    Added starting queries

---
+++ 1_queries/3_properties_by_city.sql
@@ -0,0 +1,10 @@
+SELECT properties.*,
+AVG(rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '_ancouver'
+GROUP BY properties.id
+HAVING AVG(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
