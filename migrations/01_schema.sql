3571f9f685ec5437d8fbb47b5778730684d3c24e
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-04-02 13:06:32 -0700

    Reorganized migration schema to put all tables in one file

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,7 +1,9 @@
-DROP TABLE IF EXISTs property_reviews;
-DROP TABLE IF EXISTs reservations;
-DROP TABLE IF EXISTs properties;
-DROP TABLE IF EXISTs users;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTs property_reviews CASCADE;
+DROP TABLE IF EXISTs reservations CASCADE;
+DROP TABLE IF EXISTs properties CASCADE;
+DROP TABLE IF EXISTs users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -46,6 +48,27 @@ CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
+
+
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY,
+  property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE,
+
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
 
   rating SMALLINT NOT NULL DEFAULT 0,
