b6d2ece70f2a1111711f3a81a5241619b98ac9a7
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-04-02 09:42:10 -0700

    Refactored addUser to use db query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -76,10 +76,21 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users (name, password, email)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [user.name, user.password, user.email])
+    .then(res => {
+      console.log(res.rows);
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows[0];
+    })
+    .catch(err => {
+      throw err;
+    });
 };
 exports.addUser = addUser;
 
